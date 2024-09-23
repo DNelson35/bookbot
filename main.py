1cf613c89449c0c3da3ac7ffc1cd3c01bff0da61
Damien Nelson <dhnisaman@gmail.com>
2024-09-23 14:40:29 -0400

    add count char and print report functions

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
   book = "books/frankenstein.txt"
   txt = get_book_text(book)
   total_words = count_words(txt)
-  print(total_words)
+  char_lookup = count_char(txt)
+  print_report(total_words, char_lookup)
 
 
 def get_book_text(book):
@@ -12,6 +13,27 @@ def get_book_text(book):
 def count_words(txt):
   word_list = txt.split()
   return len(word_list)
+
+def count_char(txt):
+  char_dict = {}
+  uniform_txt = txt.lower()
+  for char in uniform_txt:
+    if char in char_dict:
+      char_dict[char] += 1
+    elif char.isalpha():
+      char_dict[char] = 1
+    # lambda function for sorting by value instead of key
+  return dict(sorted(char_dict.items(), key=lambda item: item[1], reverse=True))
+
+
+
+
+def print_report(word_count, char_lookup):
+  print("--- Begin report of books/frankenstein.txt ---")
+  print(f"{word_count} words found in document\n")
+  for char, value in char_lookup.items():
+    print(f"'{char}' character was found {value} times ")
+  print("--- End report ---")
 
 
 main()
